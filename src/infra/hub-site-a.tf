ac691705d7a770780a4ba6fc7be57636b6c70a79
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-21 07:53:59 +0200

    bugfix

--- src/infra/hub-site-a.tf
+++ src/infra/hub-site-a.tf
@@ -14,35 +14,35 @@ resource "azurerm_virtual_network" "hubvneta" {
 resource "azurerm_subnet" "hubvnet_subnet_1" {
   name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.hubsitea.name
-  virtual_network_name = azurerm_virtual_network.hubsitea.name
+  virtual_network_name = azurerm_virtual_network.hubvneta.name
   address_prefixes     = ["10.1.1.0/24"]
 }
 
 resource "azurerm_subnet" "hubvnet_subnet_2" {
   name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.hubsitea.name
-  virtual_network_name = azurerm_virtual_network.hubsitea.name
+  virtual_network_name = azurerm_virtual_network.hubvneta.name
   address_prefixes     = ["10.1.2.0/24"]
 }
 
 resource "azurerm_subnet" "hubvnet_subnet_3" {
   name                 = "subnet3"
   resource_group_name  = azurerm_resource_group.hubsitea.name
-  virtual_network_name = azurerm_virtual_network.hubsitea.name
+  virtual_network_name = azurerm_virtual_network.hubvneta.name
   address_prefixes     = ["10.1.3.0/24"]
 }
 
 resource "azurerm_subnet" "hubvnet_subnet_routeserver" {
   name                 = "RouteServerSubnet"
   resource_group_name  = azurerm_resource_group.hubsitea.name
-  virtual_network_name = azurerm_virtual_network.hubsitea.name
+  virtual_network_name = azurerm_virtual_network.hubvneta.name
   address_prefixes     = ["10.1.4.0/24"]
 }
 
 resource "azurerm_subnet" "hubvnet_subnet_gateway" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.hubsitea.name
-  virtual_network_name = azurerm_virtual_network.hubsitea.name
+  virtual_network_name = azurerm_virtual_network.hubvneta.name
   address_prefixes     = ["10.1.5.0/24"]
 }
 
