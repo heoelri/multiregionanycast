38fcc0dff4ca43979f527b29d8cd35787668d38d
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-21 07:55:29 +0200

    add public ip

--- src/infra/hub-site-a.tf
+++ src/infra/hub-site-a.tf
@@ -53,11 +53,18 @@ resource "azurerm_virtual_hub" "hubsitea_vhub" {
   sku                 = "Standard"
 }
 
+resource "azurerm_public_ip" "hubsitea" {
+  name                = "${azurerm_resource_group.hubsitea.name}-pip"
+  location            = azurerm_resource_group.hubsitea.location
+  resource_group_name = azurerm_resource_group.hubsitea.name
+  allocation_method   = "Dynamic"
+}
+
 resource "azurerm_virtual_hub_ip" "hubsitea_vhub_ip" {
   name                         = "${azurerm_resource_group.hubsitea.name}-vhubipconfig"
   virtual_hub_id               = azurerm_virtual_hub.hubsitea_vhub.id
   private_ip_address           = "10.5.1.18"
   private_ip_allocation_method = "Static"
-  #public_ip_address_id         = azurerm_public_ip.hubsitea.id
+  public_ip_address_id         = azurerm_public_ip.hubsitea.id
   subnet_id                    = azurerm_subnet.hubvnet_subnet_routeserver.id
 }
