d11e07cefc0c29e442634554c3c38b44290361e7
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-21 09:10:10 +0200

    add quagga config

--- src/infra/hub-site-a.tf
+++ src/infra/hub-site-a.tf
@@ -70,6 +70,15 @@ resource "azurerm_virtual_hub_ip" "hubsitea_vhub_ip" {
   subnet_id                    = azurerm_subnet.hubvnet_subnet_routeserver.id
 }
 
+resource "azurerm_virtual_hub_bgp_connection" "hubsitea_nva_connection" {
+  name           = "${azurerm_resource_group.hubsitea.name}-vhub-bgpconnection"
+  virtual_hub_id = azurerm_virtual_hub.hubsitea_vhub.id
+  peer_asn       = 65001
+  peer_ip        = azurerm_network_interface.hubsitea_routervm_1.private_ip_address
+
+  depends_on = [azurerm_virtual_hub_ip.hubsitea_vhub_ip]
+}
+
 #
 # VM-based Router (NVA)
 #
