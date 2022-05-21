3c9d541bc8d9587895003e4ef29ac8589944b616
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-21 08:09:06 +0200

    push

--- src/infra/hub-site-a.tf
+++ src/infra/hub-site-a.tf
@@ -58,6 +58,7 @@ resource "azurerm_public_ip" "hubsitea" {
   location            = azurerm_resource_group.hubsitea.location
   resource_group_name = azurerm_resource_group.hubsitea.name
   allocation_method   = "Dynamic"
+  sku                 = "Standard"
 }
 
 resource "azurerm_virtual_hub_ip" "hubsitea_vhub_ip" {
