d8519a7c23630aedd73a3c2c1f2e721d46f08546
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-21 16:05:58 +0200

    revert back to 18.04

--- src/infra/hub-site-a.tf
+++ src/infra/hub-site-a.tf
@@ -124,7 +124,7 @@ resource "azurerm_linux_virtual_machine" "hubsitea_routervm_1" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
