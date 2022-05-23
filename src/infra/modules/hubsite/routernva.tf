ea6d37c2e3a968a2551b1c516f1cb99bc7559ed6
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-23 18:59:57 +0200

    set route server ips

--- src/infra/modules/hubsite/routernva.tf
+++ src/infra/modules/hubsite/routernva.tf
@@ -65,8 +65,8 @@ data "template_file" "cloudinit" {
     bgp_network1    = azurerm_subnet.hubvnet_subnet_1.address_prefixes[0], # first network advertised from quagga to the router server (inclusive of subnetmask)
     bgp_network2    = azurerm_subnet.hubvnet_subnet_2.address_prefixes[0], # second network advertised from quagga to the router server (inclusive of subnetmask)
     bgp_network3    = azurerm_subnet.hubvnet_subnet_3.address_prefixes[0], # third network advertised from quagga to the router server (inclusive of subnetmask)
-    routeserver_IP1 = "1.2.3.4", # first IP address of the router server 
-    routeserver_IP2 = "4.3.2.1" # second IP address of the router server
+    routeserver_IP1 = "10.1.5.4", # first IP address of the router server 
+    routeserver_IP2 = "10.1.5.5" # second IP address of the router server, 
   }
 }
 
