ac3737279e633c6f7d51b92f0b40a5a551c48c38
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-23 09:24:09 +0200

    auto-generate shared key

--- src/infra/clientsites.tf
+++ src/infra/clientsites.tf
@@ -46,6 +46,13 @@ resource "azurerm_virtual_network_gateway" "clientsitea_vpngw" {
   }
 }
 
+# generate random password
+resource "random_password" "vpn_shared_key" {
+  length           = 64
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
 # foreach? multiple hubsites
 resource "azurerm_virtual_network_gateway_connection" "clientsitea_to_hubsitea" {
   name                = "clientsitea-to-hubsitea"
@@ -56,7 +63,7 @@ resource "azurerm_virtual_network_gateway_connection" "clientsitea_to_hubsitea" 
   virtual_network_gateway_id      = azurerm_virtual_network_gateway.clientsitea_vpngw.id
   peer_virtual_network_gateway_id = module.hubsite["westeurope"].peer_virtual_network_gateway_id
 
-  shared_key = "4-v3ry-53cr37-1p53c-5h4r3d-k3y"
+  shared_key = random_password.vpn_shared_key.result
 }
 
 resource "azurerm_virtual_network_gateway_connection" "hubsitea_to_clientsitea" {
@@ -68,5 +75,5 @@ resource "azurerm_virtual_network_gateway_connection" "hubsitea_to_clientsitea" 
   virtual_network_gateway_id      = module.hubsite["westeurope"].peer_virtual_network_gateway_id
   peer_virtual_network_gateway_id = azurerm_virtual_network_gateway.clientsitea_vpngw.id
 
-  shared_key = "4-v3ry-53cr37-1p53c-5h4r3d-k3y"
+  shared_key = random_password.vpn_shared_key.result
 }
