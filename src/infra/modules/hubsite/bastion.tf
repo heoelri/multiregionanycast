8feeeeb7068fd747cabd30dc6ab50770180a4f53
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-23 10:06:40 +0200

    replace pip with bastion

---
+++ src/infra/modules/hubsite/bastion.tf
@@ -0,0 +1,19 @@
+resource "azurerm_public_ip" "hubsite_bastion_pip" {
+  name                = "${azurerm_resource_group.hubsite.name}-bastion-pip"
+  location            = azurerm_resource_group.hubsite.location
+  resource_group_name = azurerm_resource_group.hubsite.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "example" {
+  name                = "examplebastion"
+  location            = azurerm_resource_group.hubsite.location
+  resource_group_name = azurerm_resource_group.hubsite.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.hubvnet_subnet_bastion.id
+    public_ip_address_id = azurerm_public_ip.hubsite_bastion_pip.id
+  }
+}
