0d0cdc000aa6675d9a522627ed4c2846c3c22fc5
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-24 13:11:52 +0200

    substring for akv

--- src/infra/modules/hubsite/keyvault.tf
+++ src/infra/modules/hubsite/keyvault.tf
@@ -1,6 +1,6 @@
 # hub site key vault used to store secrets
 resource "azurerm_key_vault" "hubsite" {
-  name                        = "${azurerm_resource_group.hubsite.name}-kv"
+  name                        = substr("${azurerm_resource_group.hubsite.name}-kv",0,22)
   location                    = azurerm_resource_group.hubsite.location
   resource_group_name         = azurerm_resource_group.hubsite.name
   enabled_for_disk_encryption = true
