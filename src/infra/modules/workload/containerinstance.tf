a88601a240ff38840f7bb2bef8f1530ba1691871
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-24 18:37:37 +0200

    bugfix

--- src/infra/modules/workload/containerinstance.tf
+++ src/infra/modules/workload/containerinstance.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_group" "workload" {
   name                = "${azurerm_resource_group.workload.name}-ci"
   location            = azurerm_resource_group.workload.location
   resource_group_name = azurerm_resource_group.workload.name
-  ip_address_type     = "private"
+  ip_address_type     = "Private"
   dns_name_label      = "aci-label"
   os_type             = "Linux"
 
