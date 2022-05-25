b015b75a1e7e401831c699328a6a3fbe19b2826b
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-25 10:21:27 +0200

    fix outputs

--- src/infra/output.tf
+++ src/infra/output.tf
@@ -2,6 +2,22 @@ output "hubsite_westeurope_cloudinit" {
   value = module.hubsite_westeurope.custom_data
 }
 
-output "hubsite_northeurope_cloudinit" {
-  value = module.hubsite_westeurope.custom_data
+output "hubsite_westeurope_virtualRouterIP1" {
+  value = module.hubsite_westeurope.virtualRouterIP1
+}
+
+output "hubsite_westeurope_virtualRouterIP2" {
+  value = module.hubsite_westeurope.virtualRouterIP2
+}
+
+output "hubsite_swedencentral_cloudinit" {
+  value = module.hubsite_swedencentral.custom_data
+}
+
+output "hubsite_swedencentral_virtualRouterIP1" {
+  value = module.hubsite_swedencentral.virtualRouterIP1
+}
+
+output "hubsite_swedencentral_virtualRouterIP2" {
+  value = module.hubsite_swedencentral.virtualRouterIP2
 }
